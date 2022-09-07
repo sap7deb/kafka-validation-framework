8fb04b3aa8b2fe76994e0ea8785e8e57131fcc7f
Saptarshi Deb <39966991+sap7deb@users.noreply.github.com>
2022-09-07 14:54:24 +0530

    Added Extent Reports and Hooks (#2)

--- src/test/java/stepDefinitions/GivenSteps.java
+++ src/test/java/stepDefinitions/GivenSteps.java
@@ -1,14 +1,24 @@
 package stepDefinitions;
 
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
+import utils.BaseTest;
 import utils.KafkaConstants;
 
 public class GivenSteps extends KafkaConstants {
+    BaseTest baseTest;
+    Scenario scenario;
+
+    public GivenSteps(BaseTest baseTest) {
+        this.baseTest = baseTest;
+        this.scenario = baseTest.getScenario();
+    }
 
     @Given("user should generate a kafka message with movie name as {string} and genre as {string}")
     public void generateKafkaMessage(String movieName, String genre) {
         movie.setMovieName(movieName);
         movie.setGenre(genre);
+        scenario.log("Data is created: "+movie);
     }
 
 }
